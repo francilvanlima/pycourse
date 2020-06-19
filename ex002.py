5e800f67f5ff782cd1c9c4ce3db18f0edad3706c
Francilvan Lima <francilvanlima@gmail.com>
2020-06-19 15:16:33 -0300

    Update no  ex002.py

--- ex002.py
+++ ex002.py
@@ -1,4 +1,5 @@
 from colorama import init
 init()
 nome = input('Digite seu nome: ')
-print ('\033[32mBem vindo ao curso de python\033[32m, \033[35m{}!\033[m'.format(nome))
+print('\033[32mBem vindo ao curso de python\033[32m, \033[35m{}!\033[m'.format(nome))
+print('Bem vindo a Las Ensinas')
